bd3988bc8ed795ad9d3ce066454ad4b5171a8fdd
Eben <ebenezergana@gmail.com>
2022-11-25 20:10:43 +0100

    initial commit

---
+++ app/src/main/java/com/ebenezer/gana/gitdemo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ebenezer.gana.gitdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Hey look, a lot of code here... lots of them :)
+    }
+}
